d7098e1c024d453ac88889af3bde3b66d3fa057a
sangnguyens <sangnt.pvpro@vpi.pvn.vn>
2022-06-17 08:52:22 +0700

    add solution

--- assignments/week-6-auto-ml/nb/tpot_mpg_pipeline.py
+++ assignments/week-6-auto-ml/nb/tpot_mpg_pipeline.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LassoLarsCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
 from tpot.builtins import StackingEstimator
@@ -12,10 +13,10 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: -1.9535992134211184
+# Average CV score on the training set was: -1.9559407061012997
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=ExtraTreesRegressor(bootstrap=True, max_features=0.8500000000000001, min_samples_leaf=1, min_samples_split=6, n_estimators=100)),
-    ExtraTreesRegressor(bootstrap=False, max_features=0.8500000000000001, min_samples_leaf=4, min_samples_split=13, n_estimators=100)
+    StackingEstimator(estimator=LassoLarsCV(normalize=False)),
+    RandomForestRegressor(bootstrap=False, max_features=0.3, min_samples_leaf=2, min_samples_split=3, n_estimators=100)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
